e4e54e0efa942be13a2675b71bbfd4cd6f655210
klimserstanikov <ksherstyanikov@marfa-tech.com>
2022-08-20 00:37:49 +0300

    install easy admin

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractDashboardController
+{
+    #[Route('/user', name: 'users')]
+    public function index(): Response
+    {
+        echo "hello";
+    }
+}

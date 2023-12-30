5145280242f2468c90461345e773cc2e05b0decd
klim <ks.sherstyanikov@roseltorg.ru>
2023-12-30 16:06:14 +0300

    add json

---
+++ src/Controller/Admin/ShopCrudController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Shop;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class ShopCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Shop::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}

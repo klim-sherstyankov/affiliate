7dd0044cc058c2390019af4af1476e8212d7369c
klim <ks.sherstyanikov@roseltorg.ru>
2024-01-03 18:49:42 +0300

    add json

--- src/Controller/Admin/ItemsCrudController.php
+++ src/Controller/Admin/ItemsCrudController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Admin;
 use App\Entity\Items;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -20,12 +19,11 @@ class ItemsCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('shortName'),
             TextField::new('url'),
             TextEditorField::new('description'),
             IntegerField::new('price'),
-            TextField::new('salePrice'),
+            IntegerField::new('salePrice'),
             TextField::new('image'),
             IntegerField::new('priceOffPercent'),
             AssociationField::new('shopId'),

751b6edab602b0319b346347ad9debb424f5ed17
klim <ks.sherstyanikov@roseltorg.ru>
2024-01-03 15:48:20 +0300

    add json

--- public/index.php
+++ public/index.php
@@ -4,9 +4,9 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-//header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Headers", "origin, x-requested-with, content-type");
-//header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers", "origin, x-requested-with, content-type");
+header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
